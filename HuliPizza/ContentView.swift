5d500ee0c40be9fe4a5e7d3ee1aa91a410a06307
Sahit Katta <sahit.mohan@gmail.com>
2020-02-16 17:41:13 +0530

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Sahit Katta on 16/02/20.
+//  Copyright © 2020 Sahit Katta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
