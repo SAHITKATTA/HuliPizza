25fe0cb2f111ce78200276712373adf82982bd8c
Sahit Katta <sahit.mohan@gmail.com>
2020-02-16 18:09:05 +0530

    SwiftUI and Xcode Basic Concepts

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -10,12 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Huli Pizza Company")
+                .font(.title)
+            Image("Surf Board")
+                .resizable() // first resizable then scale to fit but vice versa doesnt work
+                .scaledToFit()
+            Text("Order Pizza")
+                .font(.largeTitle)
+            Spacer()
+        }
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            ContentView()
+            ContentView()
+                .colorScheme(.dark)
+                .background(Color.black)
+            .previewDevice("iPad Pro (9.7-inch)")
+            
+        }
     }
 }
+ 
